015fd3480bd65e82221c005b3407a7b239d2117c
Morlay <morlay.null@gmail.com>
2019-09-23 18:37:50 +0800

    rename condition operator

--- go.mod
+++ go.mod
@@ -1,5 +1,8 @@
-module github.com/go-courier/labelql
+module github.com/go-courier/kvcondition
 
-go 1.12
+go 1.13
 
-require github.com/stretchr/testify v1.3.0
+require (
+	github.com/davecgh/go-spew v1.1.0
+	github.com/stretchr/testify v1.4.0
+)
---
+++ kv_condition_test.go
@@ -0,0 +1,60 @@
+package kvcondition
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func BenchmarkParseKVCondition(b *testing.B) {
+	rule := []byte(`env && tag = ONLINE & tag = "some label" & ip & ( ip != 1.1.1.1 | ip ^= "8.8" | tag *= "test&" ) | ip $= 4.4`)
+
+	for i := 0; i < b.N; i++ {
+		ParseKVCondition(rule)
+	}
+}
+
+func TestParseKVCondition(t *testing.T) {
+	tt := require.New(t)
+
+	rule := []byte(`env & tag = ONLINE & tag = "some label" & ip & ( ip != 1.1.1.1 | ip ^= "8.8" | tag *= "test\&" ) | ip $= 4.4`)
+
+	kvCondition := &KVCondition{}
+	err := kvCondition.UnmarshalText(rule)
+	tt.NoError(err)
+
+	tt.Equal(
+		`( ( ( ( ( env & tag = "ONLINE" ) & tag = "some label" ) & ip ) & ( ( ip != "1.1.1.1" | ip ^= "8.8" ) | tag *= "test&" ) ) | ip $= "4.4" )`,
+		kvCondition.Node.String(),
+	)
+
+	kvc, err := ParseKVCondition([]byte(kvCondition.Node.String()))
+	tt.NoError(err)
+	tt.Equal(kvCondition.String(), kvc.String())
+
+	rules := make([]*Rule, 0)
+
+	kvc.Range(func(label *Rule) {
+		rules = append(rules, label)
+	})
+
+	tt.Equal([]*Rule{
+		OperatorExists.Of("env", ""),
+		OperatorEqual.Of("tag", "ONLINE"),
+		OperatorEqual.Of("tag", "some label"),
+		OperatorExists.Of("ip", ""),
+		OperatorNotEqual.Of("ip", "1.1.1.1"),
+		OperatorStartsWith.Of("ip", "8.8"),
+		OperatorContains.Of("tag", "test&"),
+		OperatorEndsWith.Of("ip", "4.4"),
+	}, rules)
+}
+
+func TestParseKVConditionFailed(t *testing.T) {
+	tt := require.New(t)
+
+	rule := "tag = ONLINE & tag = \"some label\" & ( ip = 1.1.1.1 | ip = \"8.8.8.8\" | tag = test & ip = 4.4.4.4"
+
+	_, err := ParseKVCondition([]byte(rule))
+	tt.Error(err)
+}
